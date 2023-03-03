a81078fcf4f81de3a22d1133f48d6898f249678f
Georgi Todorov <georgiit98@gmail.com>
2023-03-03 16:18:09 +0200

    Color Palette Generator
    Color Palete Generator

---
+++ Palette Generator/script.js
@@ -0,0 +1,38 @@
+const container = document.querySelector(".container");
+const refreshBtn = document.querySelector(".refresh-btn");
+
+const maxPaletteBoxes = 32;
+
+const generatePalette = () =>{
+    container.innerHTML = ""; //clearing the container
+    for (let i = 0; i < maxPaletteBoxes; i++) {
+        
+    // generate random Hex number
+        let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHex = `#${randomHex.padStart(6, "0")}`;
+    
+        // creating a new 'li' element and inserting it to the container
+        const color = document.createElement("li");
+        color.classList.add("color")
+        color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
+        <span class="hex-value">${randomHex}</span>`;
+
+    // adding click event to current li element to copy the color
+        color.addEventListener("click", () => copyColor(color, randomHex));
+        container.appendChild(color);
+
+    }
+}
+generatePalette();
+
+// copied the hex value and print it "COPIED" for 1 sec
+const copyColor = (elem, hexVal) => {
+const colorElement = elem.querySelector(".hex-value");
+navigator.clipboard.writeText(hexVal).then(() => {
+    colorElement.innerText = "Copied";
+    setTimeout(() => colorElement.innerText = hexVal, 1000);
+}) . catch(() => alert("Failed to copy the color code!")); // showing the alert if the color doesn't copy
+
+};
+
+refreshBtn.addEventListener("click", generatePalette)
