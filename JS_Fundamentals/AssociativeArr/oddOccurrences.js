7ee07596d4ff036ccc18452db46abc81a65fc294
Georgi Todorov <81701314+GeorgiIT@users.noreply.github.com>
2024-04-11 20:37:32 +0300

    adding files

--- JS_Fundamentals/AssociativeArr/oddOccurrences.js
+++ JS_Fundamentals/AssociativeArr/oddOccurrences.js
@@ -1,10 +1,23 @@
 function solve(input){
-    let allWords = {};
-    input.split(' ').forEach(x => allWords[x] = 0);
+    let arr = input.split(' ');
+    let occurrences = new Map();
 
-    allWords.forEach(x => allWords[x].toLowerCase());
+    for(let word of arr){
+        word = word.toLowerCase();
+        if(!occurrences.has(word)){
+            occurrences.set(word, 0);
+        }
+        occurrences.set(word, occurrences.get(word) + 1); // counter
+    }
 
+    let storage = []
+    for(let [key, value] of occurrences.entries()){
+        if(value % 2 !== 0){
+            storage.push(key);
+        }
+    }
+    console.log(storage.join(' '));
     
-    allWords.forEach(x => allWords[x] % 2 != 0);
+
 }
 solve('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
--- JS_Fundamentals/AssociativeArr/wordsTracker.js
+++ JS_Fundamentals/AssociativeArr/wordsTracker.js
@@ -1,28 +1,22 @@
 function solve(input) {
-    const [targetWords, ...wordsToSearch] = input;
-    const wordsMap = {};
+    let words = input.shift().split(' ');
 
-    // Split the target words and initialize counts to 0
-    targetWords.split(' ').forEach(word => wordsMap[word] = 0);
-
-    // Count occurrences of target words in the rest of the strings
-    for (const words of wordsToSearch) {
-        words.split(' ').forEach(word => {
-            if (wordsMap.hasOwnProperty(word)) {
-                wordsMap[word]++;
-            }
-        });
+    let wordsObj = {}
+    for(const word of words){
+        wordsObj[word] = 0;
     }
 
-    // Convert the wordsMap to an array of objects and sort by count in descending order
-    const sortedWords = Object.entries(wordsMap)
-        .map(([word, count]) => ({ word, count }))
-        .sort((a, b) => b.count - a.count);
-
-    // Print the sorted words and their counts
-    sortedWords.forEach(({ word, count }) => {
-        console.log(`${word} - ${count}`);
-    });
+    for(const word of input){
+        if(wordsObj.hasOwnProperty(word)){
+            wordsObj[word]++;
+        }
+    }
+    Object.keys(wordsObj)
+    .sort((a,b) => wordsObj[b] - wordsObj[a])
+    .forEach(el => {
+        console.log(`${el} - ${wordsObj[el]}`)
+    })
+    
 }
 
 solve([
