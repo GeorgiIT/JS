881de8f4d4f2ef12f4ce07bd5e97e54b60559012
Georgi Todorov <81701314+GeorgiIT@users.noreply.github.com>
2024-04-18 11:49:22 +0300

    adding files

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
@@ -1,4 +1,20 @@
 function attachEventsListeners() {
+    const convertButtons = document.querySelectorAll('input[type="button"]');
 
-    console.log('TODO:...');
+    convertButtons.forEach(button => {
+        button.addEventListener('click', function(){
+            const inputId = this.id.replace('btn' , '');
+            const inputValue = parseFloat(document.getElementById(inputId).value);
+
+            const days = inputValue / 24;
+            const hours = inputValue * 24;
+            const minutes = inputValue * 24 * 60;
+            const seconds = inputValue * 24 * 60 * 60;
+
+            document.getElementById('days').value = days;
+            document.getElementById('hours').value = hours;
+            document.getElementById('minutes').value = minutes;
+            document.getElementById('seconds').value = seconds;
+        })
+    })
 }
