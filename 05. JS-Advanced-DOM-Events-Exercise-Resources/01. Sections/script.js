7062a9ab62255006a39b34096780a857d52d78c5
Georgi Todorov <81701314+GeorgiIT@users.noreply.github.com>
2024-04-18 10:19:50 +0300

    Sections

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
@@ -1,3 +1,25 @@
 function create(words) {
-   console.log('TODO:...');
+   //div -> p -> string
+   const content = document.getElementById('content');
+
+   for(let i = 0; i < words.length; i++){
+      const div = document.createElement('div');
+      const p = document.createElement('p');
+      p.textContent = words[i];
+      p.style.display = 'none';
+
+
+      div.appendChild(p);
+      content.appendChild(div);
+   }
+   content.addEventListener('click', function(event){
+      if(event.target.tagName === 'DIV' || event.target.tagName === 'P'){
+         const para = event.target.children[0] || event.target;
+
+         // When we click on the element it will become visible and 
+         // when we click again it will disappear
+         const isVisible = para.style.display === 'block';
+         para.style.display = isVisible ? 'none' : 'block';
+      }
+   })
 }
