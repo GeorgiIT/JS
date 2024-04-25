ed739d9fe51866bca0a5cbcaccb89475ceec794b
Georgi Todorov <81701314+GeorgiIT@users.noreply.github.com>
2024-04-25 17:07:47 +0300

    My Cooking book

--- JS-Applications/2.async/02.Github-Repos/scripts/app.js
+++ JS-Applications/2.async/02.Github-Repos/scripts/app.js
@@ -1,18 +1,10 @@
-function loadRepos() {
+async function logFetch(url) {
 	const username = document.getElementById('username').value;
-
 	const url = `https://api.github.com/users/${username}/repos`;
-
-	fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			const ulElement = document.getElementById('repos');
-			ulElement.innerHTML = '';
-			data.forEach(r => {
-				const liElement = document.createElement('li');
-				liElement.textContent = r.full_name;
-				ulElement.appendChild(liElement);
-			});
-		})
-
+	try {
+		const response = await fetch(url);
+		console.log(await response.text());
+	} catch (err) {
+		console.log(`Error: ${err}`);
+	}
 }
